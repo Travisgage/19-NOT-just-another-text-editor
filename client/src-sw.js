63488282a49e1981183da0f898cae6297d6348d8
Travisgage <travisgagephillips@gmail.com>
2024-09-07 15:23:57 -0600

    Update src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -28,12 +28,19 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Asset caching
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker', 'image'].includes(request.destination),
+  ({ request }) => 
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'worker',
   new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, 
+        maxEntries: 60, 
       }),
     ],
   })
